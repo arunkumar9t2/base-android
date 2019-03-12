0979145ddf61e363d4a5a234d165ca97ee622249
Arunkumar <arunk.beece@gmail.com>
2019-03-12 08:24:58 +0530

    Testing artifact and group id for source dependencies

--- sample-app/src/main/java/dev/arunkumar/android/MainActivity.kt
+++ sample-app/src/main/java/dev/arunkumar/android/MainActivity.kt
@@ -1,7 +1,7 @@
 package dev.arunkumar.android
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
