37d541ca206f41a7d8f29f3f5d051912c20b86f3
Arunkumar <arunk.beece@gmail.com>
2019-07-21 02:00:23 +0800

    Fix cleanup snapshots task deleting artificats out of its scope
    Signed-off-by: Arunkumar <arunk.beece@gmail.com>

--- publish-all.sh
+++ publish-all.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+./gradlew clean
+
 # Publish all modules and their latest versions
 ./gradlew uploadArchives
 
 # Publish a snapshot of all modules
-./gradlew uploadArchives -PisSnapshot=true
+./gradlew uploadArchives -Psnapshot=true
 
 # Stage all files
 git add .
