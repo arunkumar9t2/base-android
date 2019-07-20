134c262ad230500b25f7b44373dbdd9bace33945
Arunkumar <arunk.beece@gmail.com>
2019-07-20 14:48:31 +0800

    Make cleanup snapshots a gradle task
    Signed-off-by: Arunkumar <arunk.beece@gmail.com>

--- common/src/main/java/dev/arunkumar/common/progress/ProgressTimeLatch.kt
+++ common/src/main/java/dev/arunkumar/common/progress/ProgressTimeLatch.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dev.arunkumar.common.loadinglatch
+package dev.arunkumar.common.progress
 
 import android.os.Handler
 import android.os.Looper
--- publish-all.sh
+++ publish-all.sh
@@ -3,9 +3,5 @@
 # Publish all modules and their latest versions
 ./gradlew uploadArchives
 
-# Purge all active snapshots
-echo "Cleaning existing snapshots"
-rm -rf maven-repo\snapshots\
-
 # Publish a snapshot of all modules
 ./gradlew uploadArchives -PisSnapshot=true
