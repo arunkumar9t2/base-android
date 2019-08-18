742a0667ba524a262bf8c2bf47acc0838db3ac2a
Arunkumar <arunk.beece@gmail.com>
2019-08-19 04:01:42 +0800

    Remove string resource for now
    Signed-off-by: Arunkumar <arunk.beece@gmail.com>

--- common/src/main/java/dev/arunkumar/common/context/Context.kt
+++ common/src/main/java/dev/arunkumar/common/context/Context.kt
@@ -6,7 +6,6 @@ import android.content.Context
 import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
-import dev.arunkumar.common.model.StringResource
 
 /**
  * Converts given [dp] to pixel units
@@ -26,13 +25,3 @@ fun Context.pxToDp(px: Int): Int {
 
 @ColorInt
 inline fun Context.resolveColorRes(@ColorRes id: Int): Int = ContextCompat.getColor(this, id)
-
-inline fun Context.resolveStringResource(
-    stringResource: StringResource,
-    args: Array<String> = emptyArray(),
-    resourceArgs: Array<Int> = emptyArray()
-) = when {
-    args.isNotEmpty() -> getString(stringResource.resource, stringResource.args)
-    resourceArgs.isNotEmpty() -> getString(stringResource.resource, resourceArgs.map(::getString))
-    else -> getString(stringResource.resource)
-}
--- common/src/main/java/dev/arunkumar/common/model/StringResource.kt
+++
@@ -1,9 +0,0 @@
-package dev.arunkumar.common.model
-
-import androidx.annotation.StringRes
-
-data class StringResource(
-    @param:StringRes val resource: Int,
-    val args: List<String> = emptyList(),
-    val resourceArgs: List<Int> = emptyList()
-)
