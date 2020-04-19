99f0bd56f2db438c40d5e0000589fccfb25ccf9f
Arunkumar <arunk.beece@gmail.com>
2020-04-19 15:43:04 +0800

    Add API to get closest matching vibrant color for given color
    Signed-off-by: Arunkumar <arunk.beece@gmail.com>

--- colors/src/main/java/dev/arunkumar/android/colors/Colors.kt
+++ colors/src/main/java/dev/arunkumar/android/colors/Colors.kt
@@ -3,26 +3,53 @@ package dev.arunkumar.android.colors
 import android.graphics.Color.parseColor
 import androidx.annotation.ColorInt
 import androidx.core.graphics.ColorUtils
-import java.util.*
 
-val DARK_VIBRANT_COLOR_700 = intArrayOf(
-  parseColor("#D32F2F"),
-  parseColor("#C2185B"),
-  parseColor("#7B1FA2"),
-  parseColor("#512DA8"),
-  parseColor("#303F9F"),
-  parseColor("#1976D2"),
-  parseColor("#0288D1"),
-  parseColor("#0097A7"),
-  parseColor("#00796B"),
-  parseColor("#388E3C"),
-  parseColor("#F57F17"),
-  parseColor("#E65100"),
-  parseColor("#E64A19"),
-  parseColor("#5D4037"),
-  parseColor("#616161"),
-  parseColor("#455A64")
-)
+val DARK_VIBRANT_COLOR_700 by lazy {
+  intArrayOf(
+    parseColor("#D32F2F"),
+    parseColor("#C2185B"),
+    parseColor("#7B1FA2"),
+    parseColor("#512DA8"),
+    parseColor("#303F9F"),
+    parseColor("#1976D2"),
+    parseColor("#0288D1"),
+    parseColor("#0097A7"),
+    parseColor("#00796B"),
+    parseColor("#388E3C"),
+    parseColor("#F57F17"),
+    parseColor("#E65100"),
+    parseColor("#E64A19"),
+    parseColor("#5D4037"),
+    parseColor("#616161"),
+    parseColor("#455A64")
+  )
+}
+
+val VIBRANT_COLOR_500 by lazy {
+  intArrayOf(
+    parseColor("#F44336"),
+    parseColor("#E91E63"),
+    parseColor("#9C27B0"),
+    parseColor("#673AB7"),
+    parseColor("#3F51B5"),
+    parseColor("#2196F3"),
+    parseColor("#5E35B1"),
+    parseColor("#3949AB"),
+    parseColor("#1E88E5"),
+    parseColor("#03A9F4"),
+    parseColor("#00BCD4"),
+    parseColor("#009688"),
+    parseColor("#4CAF50"),
+    parseColor("#8BC34A"),
+    parseColor("#CDDC39"),
+    parseColor("#FFEB3B"),
+    parseColor("#FFC107"),
+    parseColor("#FF9800"),
+    parseColor("#FF5722"),
+    parseColor("#795548"),
+    parseColor("#9E9E9E")
+  )
+}
 
 private fun colorDifference(@ColorInt a: Int, @ColorInt b: Int): Double {
   val aLab = DoubleArray(3)
@@ -32,11 +59,19 @@ private fun colorDifference(@ColorInt a: Int, @ColorInt b: Int): Double {
   return ColorUtils.distanceEuclidean(aLab, bLab)
 }
 
+/**
+ * Given an array of color ints, will return the color from the array that has shortest euclidean
+ * distance with the given [color]
+ */
 @ColorInt
-fun closestVibrantDarkColor(@ColorInt color: Int): Int {
-  val set: SortedMap<Double, Int> = TreeMap()
-  for (i in DARK_VIBRANT_COLOR_700.indices) {
-    set[colorDifference(color, DARK_VIBRANT_COLOR_700[i])] = i
-  }
-  return DARK_VIBRANT_COLOR_700[set[set.firstKey()]!!]
-}
+fun IntArray.closestColor(@ColorInt color: Int): Int =
+  map { arrayColor -> arrayColor to colorDifference(arrayColor, color) }
+    .sortedBy { it.second }
+    .map { it.first }
+    .first()
+
+@ColorInt
+fun closestVibrantDarkColor(@ColorInt color: Int): Int = DARK_VIBRANT_COLOR_700.closestColor(color)
+
+@ColorInt
+fun closestVibrantColor(@ColorInt color: Int): Int = VIBRANT_COLOR_500.closestColor(color)
--- colors/src/main/java/dev/arunkumar/android/colors/Palette.kt
+++ colors/src/main/java/dev/arunkumar/android/colors/Palette.kt
@@ -3,22 +3,37 @@ package dev.arunkumar.android.colors
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import androidx.palette.graphics.Palette
+import androidx.palette.graphics.Palette.Swatch
+import androidx.palette.graphics.Palette.from
 
-fun Palette.pickDarkVibrantColor(): Palette.Swatch = when {
+fun Palette.pickDarkVibrantColor(): Swatch = when {
   darkVibrantSwatch != null -> darkVibrantSwatch!!
   else -> {
     val dominantSwatch = dominantSwatch!!
-    Palette.Swatch(
-      closestVibrantDarkColor(dominantSwatch.rgb),
-      dominantSwatch.population
-    )
+    Swatch(closestVibrantDarkColor(dominantSwatch.rgb), dominantSwatch.population)
   }
 }
 
-fun Bitmap.bestVibrantDarkColor(): Palette.Swatch {
-  return Palette.from(this).generate().pickDarkVibrantColor()
+fun Bitmap.bestVibrantDarkColor(): Swatch {
+  return from(this).generate().pickDarkVibrantColor()
 }
 
-fun BitmapDrawable.bestVibrantDarkColor(): Palette.Swatch {
+fun BitmapDrawable.bestVibrantDarkColor(): Swatch {
   return bitmap.bestVibrantDarkColor()
+}
+
+fun Palette.pickVibrantColor(): Swatch = when {
+  vibrantSwatch != null -> vibrantSwatch!!
+  else -> {
+    val dominantSwatch = dominantSwatch!!
+    Swatch(closestVibrantColor(dominantSwatch.rgb), dominantSwatch.population)
+  }
+}
+
+fun Bitmap.bestVibrantColor(): Swatch {
+  return from(this).generate().pickVibrantColor()
+}
+
+fun BitmapDrawable.bestVibrantColor(): Swatch {
+  return bitmap.bestVibrantColor()
 }
